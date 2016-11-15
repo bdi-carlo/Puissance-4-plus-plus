e745c473960569261c0fcc776677d802283be515
bdi-carlo <benjamin.97@live.fr>
2016-11-15 18:14:23 +0100

    Add files via upload

--- puissance_classique.c
+++ puissance_classique.c
@@ -5,6 +5,7 @@
 #define M 7
 #define L 20
 
+//Demande aux joueurs leur pseudo
 void pseudo_classique(char pseudo1[L], char pseudo2[L]){
 	system("clear");
 	printf("\nVeuillez choisir vos pseudos :\n");
@@ -16,10 +17,22 @@ void pseudo_classique(char pseudo1[L], char pseudo2[L]){
 	scanf("%s", pseudo2);
 }
 
+//Enregistrement du score du gagnant
+void enregistrement_score_classique(char joueur[L], int nb_coups){
+		FILE * fichier;
+
+		fichier = fopen("best_classique.txt", "a");
+		fprintf(fichier, "%s	%i\n", joueur, nb_coups);
+		fclose(fichier);
+}
+
+//Programme permettant la construction du jeu Puissance 4 classique
 void puissance_classique(){
 	int grille[N][M];
 	int colonne, ligne, pions, nb_pions, num_joueur;
 	char joueur1[L],joueur2[L];
+
+/****************************** INITIALISATION ****************************************/
 
 	//Initialisation des pseudos
 	pseudo_classique(joueur1, joueur2);
@@ -31,9 +44,8 @@ void puissance_classique(){
 	afficher_matrice(grille);
 	
 	nb_pions = 21;
-	pions = 0;
+	pions = 1;
 	while(pions < nb_pions){
-		pions ++;
 
 /****************************** JOUEUR 1 JOUE *****************************************/
 
@@ -63,9 +75,10 @@ void puissance_classique(){
 
 		//Affichage du numéro du tour
 		system("clear");
-		printf("\nTOUR NUMERO %i\n",pions+1);
+		pions ++;
+		printf("\nTOUR NUMERO %i\n",pions);
 
-		//Placement du prion et mise à jour de la grille
+		//Placement du pion et mise à jour de la grille
 		placer_pions(grille, colonne, num_joueur);
 		afficher_matrice(grille);
 
@@ -73,12 +86,23 @@ void puissance_classique(){
 		if(gagne(grille) == 2)break;
 	}
 
-	//Affichage du résultat de la partie
+/******************** Affichage du résultat de la partie ******************************/
+
 	system("clear");
 
 	if(gagne(grille) == 0)printf("\nMATCH NUL\n\n");
 
-	else if(gagne(grille) == 1)printf("\n%s GAGNE\n\n",joueur1);
+	else if(gagne(grille) == 1){
+		printf("\n%s GAGNE\n\n",joueur1);
 
-	else if(gagne(grille) == 2)printf("\n%s 2 GAGNE\n\n",joueur2);
+		//Enregistre le score du joueur 1
+		enregistrement_score_classique(joueur1, pions);
+	}
+
+	else if(gagne(grille) == 2){
+		printf("\n%s 2 GAGNE\n\n",joueur2);
+
+		//Enregistre le score du joueur 2
+		enregistrement_score_classique(joueur2, pions);
+	}
 }
