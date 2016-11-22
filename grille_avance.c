d4a7498d949f6ffa41f24442a4a760c72ad91de9
Arthhur <arthur.prodhommeyahoo.fr>
2016-11-22 16:00:17 +0100

    joueur1

--- grille_avance.c
+++ grille_avance.c
@@ -51,10 +51,83 @@ void placer_pions_avance(t_piece grille[N][M], int colonne, int ligne, int num_j
 
 	colonne--;
 
+	
+
 	if(num_joueur == 1){
-		if(type == 1){
-			grille[ligne][colonne].valeur_pion1 = 1;
-			grille[ligne][colonne].type1 = creuse;
+		int bloquante = 2 ;
+
+		if(type == 1){		// pièce creuse
+			if(grille[ligne-1][colonne] == 0) {		// case du dessous est vide
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = creuse;
+			}
+			else if(grille[ligne-1][colonne] == 1) {		// case du dessous est creuse
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = creuse;
+			}
+			else if(grille[ligne-1][colonne] == 2) {		// case du dessous est pleine
+				grille[ligne][colonne].valeur_pion1 = 0;
+				grille[ligne][colonne].type1 = vide;
+				grille[ligne-1][colonne].valeur_pion2 = 1;
+				grille[ligne-1][colonne].type1 = pleine ;
+				grille[ligne-1][colonne].type2 = creuse;
+				
+			}
+			else if(grille[ligne-1][colonne] == 3) {		// case du dessous est bloquante
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = creuse;
+			}
 		}
+		if(type == 2){		// pièce pleine
+			if(grille[ligne-1][colonne] == 0) {		// case du dessous est vide
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = pleine;
+			}
+			else if(grille[ligne-1][colonne] == 1) {		// case du dessous est creuse
+				grille[ligne][colonne].valeur_pion1 = 0;
+				grille[ligne][colonne].type1 = vide;
+				grille[ligne-1][colonne].valeur_pion2 = 1;
+				grille[ligne-1][colonne].type1 = creuse ;
+				grille[ligne-1][colonne].type2 = pleine;
+			}
+			else if(grille[ligne-1][colonne] == 2) {		// case du dessous est pleine
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = pleine;
+				
+			}
+			else if(grille[ligne-1][colonne] == 3) {		// case du dessous est bloquante
+				grille[ligne][colonne].valeur_pion1 = 1;
+				grille[ligne][colonne].type1 = pleine;
+			}
+		}
+		if(type == 3){		// pièce bloquante
+			while (bloquante != 0) {
+				if(grille[ligne-1][colonne] == 0) {		// case du dessous est vide
+					grille[ligne][colonne].valeur_pion1 = 1;
+					grille[ligne][colonne].type1 = bloquante;
+					bloquante -- ;
+				}
+				else if(grille[ligne-1][colonne] == 1) {		// case du dessous est creuse
+					grille[ligne][colonne].valeur_pion1 = 1;
+					grille[ligne][colonne].type1 = bloquante;
+					bloquante -- ;
+				}
+				else if(grille[ligne-1][colonne] == 2) {		// case du dessous est pleine
+					grille[ligne][colonne].valeur_pion1 = 1;
+					grille[ligne][colonne].type1 = bloquante;
+					bloquante -- ;
+				
+				}
+				else if(grille[ligne-1][colonne] == 3) {		// case du dessous est bloquante
+					grille[ligne][colonne].valeur_pion1 = 1;
+					grille[ligne][colonne].type1 = bloquante;
+					bloquante -- ;
+				}
+			}
+			if(bloquante == 0) printf("Vous avez utilisé vos 2 pièces bloquantes\n") ;
+		}
+		
 	}
+
+
 }
